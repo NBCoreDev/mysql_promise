7d757d10de27d2ef71813c7f715fe5f144b054df
Utyfua <utyfua@gmail.com>
2018-01-31 19:25:28 +0200

    Fix reconnect

--- index.js
+++ index.js
@@ -2,47 +2,56 @@ var Mysql=require('mysql');
 var connecter=(config)=>{
     var connection=Mysql.createConnection(config);
     var tmp_wait;
-    connection.on('error', function(q) {
+    var f_err=function(q) {
         if(q.code==='PROTOCOL_CONNECTION_LOST'){
             tmp_wait=[];
             //connection=Mysql.createConnection(config);
-            console.debug('mysql reconnect',q)
-            return connection.connect(err=>{
+            console.debug('mysql reconnect',q);
+            var old_conn=connection;
+            connection=Mysql.createConnection(config);
+            connection.connect(err=>{
                 if(err)throw err;
                 tmp_wait.forEach(c=>c());
                 tmp_wait=null;
             });
+            old_conn.destroy();
+            return;
         }
         console.error('mysql global error',q)
-    });
-    var local_escape;
+    };
+    var f_init=()=>{
+        connection.on('error', f_err);
+        connection.config.queryFormat=function(query,values){
+            if(!values)return query;
+            return query.replace(/\:(\w+)/g,function (txt,key){
+                return values.hasOwnProperty(key)?mysqlCC.escape(values[key]):txt;
+            }.bind(this));};
+    };
     var secured=q=>q;
+    var mysqlCC=secured(function(...ar){
+        for(var i=0;i<ar.length;i++){
+            if(typeof ar=='function')
+                return connection.query(...ar)
+        }
+        return new Promise(resolve=>{
+            if(tmp_wait)tmp_wait.push(resolve);
+            else resolve();
+        }).then(()=>new Promise(resolve=>{
+            ar[ar.length]=(e,r,i)=>{
+                if(e)console.error('mysql',{e,i:{req:ar}})
+                if(!r)r=[];
+                //TODO: create prototype mysql response
+                return resolve(r)
+            }
+            connection.query(...ar)
+        }));
+    });
+    connection.on('error', f_err);
+    var local_escape;
     return new Promise((c)=>connection.connect(c))
         .then(err=>{
             if(err)throw err;
-            connection.config.queryFormat=function(query,values){
-                if(!values)return query;
-                return query.replace(/\:(\w+)/g,function (txt,key){
-                    return values.hasOwnProperty(key)?mysqlCC.escape(values[key]):txt;
-                }.bind(this));};
-            var mysqlCC=secured(function(...ar){
-                for(var i=0;i<ar.length;i++){
-                    if(typeof ar=='function')
-                        return connection.query(...ar)
-                }
-                return new Promise(resolve=>{
-                    if(tmp_wait)tmp_wait.push(resolve);
-                    else resolve();
-                }).then(()=>new Promise(resolve=>{
-                    ar[ar.length]=(e,r,i)=>{
-                        if(e)console.error('mysql',{e,i:{req:ar}})
-                        if(!r)r=[];
-                        //TODO: create prototype mysql response
-                        return resolve(r)
-                    }
-                    connection.query(...ar)
-                }));
-            });
+            f_init();
             mysqlCC.escape=mysqlCC.e=secured(v=>{
                 if(local_escape)v=local_escape(v);
                 return Mysql.escape(v);
