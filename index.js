602c27749c197b97af2b52dcc6afc951d030e6fa
Utyfua <utyfua@gmail.com>
2018-12-16 14:20:06 +0200

    queryFormat auto shift fix

--- index.js
+++ index.js
@@ -24,13 +24,36 @@ var connecter=(config,module_name)=>{
     };
     var f_init=()=>{
         connection.on('error', f_err);
-		var queryFormat=connection.config.queryFormat;
-        connection.config.queryFormat=function(query,values){
-            if(!values)return query;
-			if(values.length&&values[0])return queryFormat(query,values);
-            return query.replace(/\:(\w+)/g,function (txt,key){
-                return values.hasOwnProperty(key)?mysqlCC.escape(values[key]):txt;
-            }.bind(this));};
+        connection.config.queryFormat=function(query, values) {
+			if (!values) return query;
+			if (Array.isArray(values)) {
+				let offset = 0;
+				return query.replace(
+					/(\?{1,2})/g,
+					function(text, match) {
+						offset++;
+
+						if (offset > values.length) return text;
+
+						if (match.length === 2) return this.escapeId(values[offset - 1]);
+						else return this.escape(values[offset]);
+					}.bind(this),
+				);
+			}
+			const escaped = query.replace(
+				/\:(\w+)/g,
+				function(text, key) {
+					if (values.hasOwnProperty(key))
+						return this.escape(values[key]);
+					return text;
+				}.bind(this),
+			);
+			return escaped.replace(/\[(\w+)\]/g, function(text, key) {
+			if (values.hasOwnProperty(key))
+				return this.escapeId(values[key]);
+			return text;
+			});
+		};
     };
     var secured=q=>q;
     var mysqlCC=secured(function(...ar){
